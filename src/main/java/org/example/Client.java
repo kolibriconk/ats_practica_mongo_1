4e5009bfb7530160f76e72fb94e1fd3ff50ca127
1565479 - Jose A. Ramos Andrades <joseantonio.ramosa@autonoma.cat>
2022-05-12 11:41:15 +0200

    Acquired connection

--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -1,24 +1,32 @@
 package org.example;
 
-import com.mongodb.MongoClient;
+import com.mongodb.MongoException;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 
+import javax.print.Doc;
 import java.net.UnknownHostException;
+import java.util.Collection;
 
 public class Client {
 
-    static MongoClient mongoClient;
+    static final String URI = "mongodb://localhost:27017/";
 
     public static void main(String[] args) {
         // Create a new client
-        connect();
+        try (MongoClient mongoClient = MongoClients.create(URI)) {
+            MongoDatabase database = mongoClient.getDatabase("test");
+            MongoCollection<Document> collection = database.getCollection("companies");
+            exercise1(collection);
+        } catch (MongoException ignored) {
+            System.out.println("Cannot connect to mongo host");
+        }
     }
 
-    private static void connect() {
-        try {
-            mongoClient = new MongoClient();
+    private static void exercise1(MongoCollection<Document> collection) {
 
-        } catch (UnknownHostException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
